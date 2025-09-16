b40797b7413ee1648f5101e66c6a03e1d22b4666
大嶋達也 <nagaitatsuya@nagaitatsuyanoMacBook-Air.local>
2025-09-16 16:07:20 +0900

    Initial bot commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const restify = require('restify');
+const { BotFrameworkAdapter } = require('botbuilder');
+
+const adapter = new BotFrameworkAdapter({
+  appId: process.env.MicrosoftAppId,
+  appPassword: process.env.MicrosoftAppPassword
+});
+
+const server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, () => {
+  console.log(`Bot is listening`);
+});
+
+server.post('/api/messages', async (req, res) => {
+  await adapter.processActivity(req, res, async (context) => {
+    await context.sendActivity(`こんにちは！あなたは「${context.activity.text}」と言いましたね。`);
+  });
+});
